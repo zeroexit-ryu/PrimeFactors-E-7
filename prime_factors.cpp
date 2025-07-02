d692dc14a62ed165e67ea2f06270bafed09e8439
YoungJae Ryu <zeroexit@gmail.com>
2025-07-02 10:49:44 +0900

    [refactoring] p67

--- prime_factors.cpp
+++ prime_factors.cpp
@@ -6,20 +6,14 @@ class PrimeFactors {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
-		if (number > 1) {
-			int divisor = 2;
-			if (number == 4 || number == 6 || number == 9 || number == 12) {
-				for (divisor = 2; number > 1; divisor++) {
-					while (number % divisor == 0) {
-						result.push_back(divisor);
-						number /= divisor;
-					}
-				}
+
+		for (int divisor = 2; number > 1; divisor++) {
+			while (number % divisor == 0) {
+				result.push_back(divisor);
+				number /= divisor;
 			}
-			else {
-				result.push_back(number);
-			}			
 		}
+
 		return result;
 	}
 };
