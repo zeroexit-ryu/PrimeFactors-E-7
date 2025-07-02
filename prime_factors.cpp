e99a8687dc4924b23984bbf7af3b434d32ecb0cf
YoungJae Ryu <zeroexit@gmail.com>
2025-07-02 10:20:44 +0900

    [feature] p52

--- prime_factors.cpp
+++ prime_factors.cpp
@@ -7,7 +7,13 @@ public:
 	vector<int> of(int number) {
 		vector<int> result = {};
 		if (number > 1) {
-			result.push_back(number);
+			if (number == 4) {
+				result.push_back(2);
+				result.push_back(2);
+			}
+			else {
+				result.push_back(number);
+			}			
 		}
 		return result;
 	}
