fd8bd7c21e3ccc5bc5d22b5c2e0d694a19a9fa37
YoungJae Ryu <zeroexit@gmail.com>
2025-07-02 10:36:31 +0900

    [refactoring] p53

--- prime_factors.cpp
+++ prime_factors.cpp
@@ -8,8 +8,15 @@ public:
 		vector<int> result = {};
 		if (number > 1) {
 			if (number == 4) {
-				result.push_back(2);
-				result.push_back(2);
+				if (number % 2 == 0) {
+					result.push_back(2);
+					number /= 2;
+				}
+
+				if (number % 2 == 0) {
+					result.push_back(2);
+					number /= 2;
+				}				
 			}
 			else {
 				result.push_back(number);
