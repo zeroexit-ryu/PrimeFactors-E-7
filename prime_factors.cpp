f83a495f5be384360f6ee4f7904b5cf8f308159c
YoungJae Ryu <zeroexit@gmail.com>
2025-07-02 10:47:17 +0900

    [refactoring] p64

--- prime_factors.cpp
+++ prime_factors.cpp
@@ -8,13 +8,7 @@ public:
 		vector<int> result = {};
 		if (number > 1) {
 			int divisor = 2;
-			if (number == 4) {
-				while (number % divisor == 0) {
-					result.push_back(divisor);
-					number /= divisor;
-				}
-			}
-			else if (number == 6 || number == 9) {
+			if (number == 4 || number == 6 || number == 9) {
 				for (divisor = 2; number > 1; divisor++) {
 					while (number % divisor == 0) {
 						result.push_back(divisor);
