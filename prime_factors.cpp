2babcef722f4c73e592d87c12763764c8e55a5b2
YoungJae Ryu <zeroexit@gmail.com>
2025-07-02 10:46:47 +0900

    [refactoring] p63

--- prime_factors.cpp
+++ prime_factors.cpp
@@ -14,17 +14,13 @@ public:
 					number /= divisor;
 				}
 			}
-			else if (number == 6) {
+			else if (number == 6 || number == 9) {
 				for (divisor = 2; number > 1; divisor++) {
 					while (number % divisor == 0) {
 						result.push_back(divisor);
 						number /= divisor;
 					}
 				}
-			}
-			else if (number == 9) {
-				result.push_back(3);
-				result.push_back(3);
 			}
 			else {
 				result.push_back(number);
