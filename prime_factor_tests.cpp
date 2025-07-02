5984f67fc4591968ae5eab33f6d72a28d72a6c42
YoungJae Ryu <zeroexit@gmail.com>
2025-07-02 10:13:15 +0900

    [feature] p45

--- prime_factor_tests.cpp
+++ prime_factor_tests.cpp
@@ -6,3 +6,9 @@ TEST(PrimeFactors, 0f1) {
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
+
+TEST(PrimeFactors, 0f2) {
+	PrimeFactors prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
--- prime_factors.cpp
+++ prime_factors.cpp
@@ -4,8 +4,11 @@ using std::vector;
 
 class PrimeFactors {
 public:
-	vector<int> of(int i) {
+	vector<int> of(int number) {
 		vector<int> result = {};
+		if (number == 2) {
+			result.push_back(2);
+		}
 		return result;
 	}
 };
