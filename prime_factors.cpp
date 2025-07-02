61bbc86d3cf746eaf94c781e4ded402c148c5912
YoungJae Ryu <zeroexit@gmail.com>
2025-07-02 10:41:01 +0900

    [refactoring] p59

--- prime_factors.cpp
+++ prime_factors.cpp
@@ -7,16 +7,17 @@ public:
 	vector<int> of(int number) {
 		vector<int> result = {};
 		if (number > 1) {
+			int divisor = 2;
 			if (number == 4) {
-				while (number % 2 == 0) {
-					result.push_back(2);
-					number /= 2;
+				while (number % divisor == 0) {
+					result.push_back(divisor);
+					number /= divisor;
 				}
 			}
 			else if (number == 6) {
-				while (number % 2 == 0) {
-					result.push_back(2);
-					number /= 2;
+				while (number % divisor == 0) {
+					result.push_back(divisor);
+					number /= divisor;
 				}
 
 				while (number % 3 == 0) {
